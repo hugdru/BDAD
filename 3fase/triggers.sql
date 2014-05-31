cd892951133940392c12d2f1d9976f038a3fc5f3
T07 <nodoubt@netcabo.pt>
2014-05-31 21:23:41 +0000

    Adicionei um trigger é preciso verificar o código

--- 3fase/triggers.sql
+++ 3fase/triggers.sql
@@ -41,3 +41,51 @@ BEGIN
 END;
 
 
+-- 2º Trigger - Um arbitro não poder ser jogador numa partida ( A ideia era dizer que a mesma pessoa nao poder ser arbitro e jogador na mesma partida
+-- podiamos ate espandir a ideia para o torneio.
+-- uma forma de ver se e a mesma pessoa foi comprar o nome E o telefone , duas pessoas diferentes nao vao ter dois dados iguais
+
+CREATE TRIGGER PessoaArbitroOuJogadorNoTorneio
+BEFORE INSERT ON ArbitroPartida
+BEGIN
+  SELECT CASE
+    WHEN
+      (
+        ( SELECT *
+          FROM Arbitro, Partida, EquipaPartida, JogadorEquipa
+          WHERE
+			Arbitro.idArbitro = new.idArbitro AND
+			Partida.idPartida = new.idPartida AND
+			Partida.idEquipa = EquipaPartida.idEquipa AND
+			JogadorEquipa.idEquipa =  EquipaPartida.idEquipa AND
+			!(JogadorEquipa.idJogador.nome == Arbitro.idArbitro.nome AND 
+			JogadorEquipa.idJogador.telefone == Arbitro.idArbitro.telefone)
+		) IS NULL
+	)
+	THEN RAISE ( ABORT , 'Ests arbitro nao pode arbitrar esta partida porque e jogador')
+	END;
+END;
+
+---???
+
+CREATE TRIGGER PessoaArbitroOuJogadorNoTorneio
+BEFORE UPDATE ON ArbitroPartida
+BEGIN
+  SELECT CASE
+    WHEN
+      (
+        ( SELECT *
+          FROM Arbitro, Partida, EquipaPartida, JogadorEquipa
+          WHERE
+			Arbitro.idArbitro = new.idArbitro AND
+			Partida.idPartida = new.idPartida AND
+			Partida.idEquipa = EquipaPartida.idEquipa AND
+			JogadorEquipa.idEquipa =  EquipaPartida.idEquipa AND
+			!(JogadorEquipa.idJogador.nome == Arbitro.idArbitro.nome AND 
+			JogadorEquipa.idJogador.telefone == Arbitro.idArbitro.telefone)
+		) IS NULL
+	)
+	THEN RAISE ( ABORT , 'Ests arbitro nao pode arbitrar esta partida porque e jogador')
+	END;
+END;
+
